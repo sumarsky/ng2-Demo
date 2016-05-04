a756f54b85e440d3c91706cb082b6a03ac69566f
sumarsky <i.dimcevski@gmail.com>
2016-05-04 15:03:52 +0200

    init http

--- http/app.ts
+++ http/app.ts
@@ -1,6 +1,12 @@
 import { bootstrap } from "angular2/platform/browser";
 import { Component, Input } from "angular2/core";
 
+@Component({
+    selector: "my-app",
+    template: `
+        <div>hello world</div>
+    `
+})
 class MyApp {
     constructor() { }
 }
